a49224c18ee009bc7a341e4e94fd7eb188614359
Vanessahenz-eng <qualidade16@zagonel.com.br>
2026-06-11 07:23:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ GITHUB_REPO  = os.environ.get('GITHUB_REPO', '')
 SETORES = {
     'B2-03':   {'nome': 'Apoio B2-03', 'cor': '#2563EB', 'colaboradores': {'Juliana': 30, 'Sirlei': 30, 'Danmari': 29}, 'campos_executor': ['Executor', 'Executor do teste']},
     'B1-01':   {'nome': 'Apoio B1-01', 'cor': '#059669', 'colaboradores': {'Luana': 28, 'Bruna': 27},
-                   'campos_executor': ['Responsável pela conferência', 'Executor', 'Executor do teste'],
+                   'campos_executor': ['Responsável pela conferência', 'Executor', 'Executor do teste', 'Nome do Inspetor', 'Nome do inspetor'],
                    'campo_atividade': 'Etapa Auditada',
                    'campo_tipo': 'Etapa Auditada',
                    'tipo_no_campo_atividade': True,
